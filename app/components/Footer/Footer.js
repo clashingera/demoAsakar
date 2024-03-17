c9e879173893ab6415ea9a587faa8c98d41b99bd
chutiya <clashingera2003@gmail.com>
2024-03-17 20:22:05 +0530

    line madhle change

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import Style  from  './Footer.module.css';
 function Footer() {
   return (
     <>
-    
+    <p>ggfjgf</p>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link
